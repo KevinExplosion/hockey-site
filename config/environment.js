ccf5054de39ae31055936ebdffa9359364fcf729
kevin <mattisonii@yahoo.com>
2016-03-31 14:49:27 -0700

    Link page to articles firebase, and create articles-tile

--- config/environment.js
+++ config/environment.js
@@ -5,7 +5,7 @@ module.exports = function(environment) {
     modulePrefix: 'hockey-site',
     environment: environment,
     contentSecurityPolicy: { 'connect-src': "'self' https://auth.firebase.com wss://*.firebaseio.com" },
-    firebase: 'https://YOUR-FIREBASE-NAME.firebaseio.com/',
+    firebase: 'https://kevin-hockey-blog.firebaseio.com/',
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
